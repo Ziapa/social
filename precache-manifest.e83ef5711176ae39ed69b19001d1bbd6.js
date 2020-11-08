5a19eaecaeaba58efc6005c46a71a8b12c69f066
Ziapa <SmiT4eggg@bk.ru>
2020-11-08 18:17:38 +0300

    Updates

---
+++ precache-manifest.e83ef5711176ae39ed69b19001d1bbd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a987774c7406690d3c8173bba72cb783",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "62f902193992c3c2a636",
+    "url": "/social/static/css/main.416f8f06.chunk.css"
+  },
+  {
+    "revision": "6a7b21b106f221c9e71f",
+    "url": "/social/static/js/2.7b0c002e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/social/static/js/2.7b0c002e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f902193992c3c2a636",
+    "url": "/social/static/js/main.5d1ce02d.chunk.js"
+  },
+  {
+    "revision": "245b1d1fb60e0de385d4",
+    "url": "/social/static/js/runtime-main.3c0aa0bb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social/precache-manifest.3ab28e49c5767ee9aa9b4fbf9ecd034c.js"
+  "/social/precache-manifest.e83ef5711176ae39ed69b19001d1bbd6.js"
 );
 
 self.addEventListener('message', (event) => {
